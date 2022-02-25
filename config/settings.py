fff70de11a4b2b200e19301504d2d296a9747895
Kevin Bowen <kevin.bowen@gmail.com>
2022-02-25 10:04:59 -0800

    Add logout link

--- config/settings.py
+++ config/settings.py
@@ -126,4 +126,5 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL='home'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
