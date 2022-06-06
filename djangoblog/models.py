90e1a5ee02acb668b96116d2789d21a0f57c5992
Kevin Bowen <kevin.bowen@gmail.com>
2022-06-06 14:59:13 -0700

    Add author & timestamp to posts. Fixes #18

--- djangoblog/models.py
+++ djangoblog/models.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         "auth.User",
         on_delete=models.CASCADE,
