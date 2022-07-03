f03d07c2032d6f5b0e0ae302b7347d42fdce72ca
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-02 17:57:16 -0700

    Point to update AUTH_MODEL

--- djangoblog/models.py
+++ djangoblog/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
@@ -6,7 +7,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
-        "auth.User",
+        get_user_model(),
         on_delete=models.CASCADE,
     )
     body = models.TextField()
