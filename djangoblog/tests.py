45a1be3159274a61341892c1830c75d81aa2a2b1
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-04 10:19:05 -0700

    Simplify str test

--- djangoblog/tests.py
+++ djangoblog/tests.py
@@ -20,11 +20,10 @@ class BlogTests(TestCase):
             author=self.user,
         )
 
-    def test_string_representation(self):
-        post = Post(title="A sample title")
-        self.assertEqual(str(post), post.title)
+    def test_post___str__(self):
+        self.assertEqual(str(self.post), self.post.title)
 
-    def test_get_absolute_url(self):
+    def test_post_get_absolute_url(self):
         self.assertEqual(self.post.get_absolute_url(), "/post/1/")
 
     def test_post_content(self):
