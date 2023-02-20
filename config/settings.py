2c7143ddfe4af6f22dc1cdcaeefdcb3914441502
Kevin Bowen <kevin.bowen@gmail.com>
2023-02-20 09:39:22 -0800

    Upgrade crispy-forms to 2.0. Resolves #201

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.sites",
     # Third-party applications
     "crispy_forms",
+    "crispy_bootstrap4",
     # "debug_toolbar",
     "django_countries",
     "django_extensions",
@@ -182,6 +183,12 @@ CSRF_COOKIE_SECURE = env.bool("DJANGO_CSRF_COOKIE_SECURE", default=True)
 
 # django-crispy-forms
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+CRISPY_CLASS_CONVERTERS = {
+    "textinput": "textinput textInput",
+    "fileinput": "fileinput fileUpload",
+    "passwordinput": "textinput textInput",
+}
 
 ADMINS = [("Kevin Bowen", "kevinbowen@protonmail.com")]
 MANAGERS = ADMINS
