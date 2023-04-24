44213c3372bca7a6e331b2d905326fab88860f90
Kevin Bowen <kevin.bowen@gmail.com>
2023-04-24 16:21:09 -0700

    Update MEDIA_ROOT

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,7 @@ STATICFILES_FINDERS = [
 ]
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = str(BASE_DIR.joinpath("media"))
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
