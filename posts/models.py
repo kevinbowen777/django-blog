aa43fe3c89f63791b1ad9068f3e466eb1b32e917
Kevin Bowen <kevin.bowen@gmail.com>
2023-05-23 18:34:02 -0700

    Update with new fields & index

--- posts/models.py
+++ posts/models.py
@@ -1,22 +1,33 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from django.urls import reverse
+from django.utils import timezone
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    date = models.DateTimeField(auto_now_add=True)
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
     author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
+        related_name="blog_posts",
     )
     body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2, choices=Status.choices, default=Status.DRAFT
+    )
 
     class Meta:
-        ordering = ["-date"]
+        ordering = ["-publish"]
+        indexes = [
+            models.Index(fields=["-publish"]),
+        ]
 
     def __str__(self):
         return self.title
-
-    def get_absolute_url(self):
-        return reverse("post_detail", args=[str(self.id)])
