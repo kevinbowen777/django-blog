efb2fda2982a72f4669e7f85dbefeb9ddc1bdc55
Kevin Bowen <kevin.bowen@gmail.com>
2023-05-31 18:07:24 -0700

    Convert to post_list function. Extract pagination to separate template. Resolves #269

--- posts/views.py
+++ posts/views.py
@@ -1,20 +1,26 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.core.mail import send_mail
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
 from django.views.decorators.http import require_POST
-from django.views.generic import ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from .forms import CommentForm, EmailPostForm
 from .models import Post
 
 
-class PostListView(ListView):
-    queryset = Post.published.all()
-    context_object_name = "posts"
-    paginate_by = 3
-    template_name = "posts/post_list.html"
+def post_list(request):
+    post_list = Post.published.all()
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get("page", 1)
+    try:
+        posts = paginator = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request, "posts/post_list.html", {"posts": posts})
 
 
 def post_detail(request, year, month, day, post):
