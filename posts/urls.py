72f102de0a8ca022c41d59ac54c7bb146abd77ec
Kevin Bowen <kevin.bowen@gmail.com>
2023-06-12 20:53:05 -0700

    Re-order paths

--- posts/urls.py
+++ posts/urls.py
@@ -13,18 +13,18 @@ from .views import (
 )
 
 urlpatterns = [
-    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
-    path("posts/new/", PostCreateView.as_view(), name="post_new"),
+    path("posts/", post_list, name="post_list"),
     path(
         "posts/<int:year>/<int:month>/<int:day>/<slug:post>/",
         post_detail,
         name="post_detail",
     ),
+    path("posts/new/", PostCreateView.as_view(), name="post_new"),
     path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post_edit"),
-    path("posts/", post_list, name="post_list"),
-    path("posts/tag/<slug:tag_slug>/", post_list, name="post_list_by_tag"),
     path("posts/<int:post_id>/share/", post_share, name="post_share"),
-    path("posts/<int:post_id>/comment/", comment_add, name="comment_add"),
     path("feed/", LatestPostsFeed(), name="post_feed"),
     path("search/", post_search, name="post_search"),
+    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
+    path("posts/tag/<slug:tag_slug>/", post_list, name="post_list_by_tag"),
+    path("posts/<int:post_id>/comment/", comment_add, name="comment_add"),
 ]
