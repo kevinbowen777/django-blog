d53c8cd162207da8f7fa281d0ad70c8660ea513a
Kevin Bowen <kevin.bowen@gmail.com>
2023-09-19 15:46:44 -0700

    Update rss & sitemap tests. Resolves #131 (#374)

--- conftest.py
+++ conftest.py
@@ -1,7 +1,8 @@
 import pytest
-from django.test import RequestFactory
+from django.test import Client, RequestFactory
 
 from accounts.tests.factories import UserFactory
+from posts.models import Post
 from posts.tests.factories import CommentFactory, PostFactory
 
 
@@ -28,3 +29,25 @@ def post():
 @pytest.fixture
 def comment():
     return CommentFactory()
+
+
+@pytest.fixture
+def client():
+    return Client()
+
+
+# Create posts for pagination tests
+@pytest.fixture
+def ten_posts(user):
+    posts = []
+    for post_id in range(10):
+        post_id += 1
+        Post.objects.create(
+            title="A Tiny Test Blog Post {0}".format(post_id),
+            tags="dummy, test, django, blog",
+            # slug="2023/9/18/a-tiny-test-blog-post-{0}/".format(post_id),
+            body="Some blog post content {0}".format(post_id),
+            author=user,
+        )
+        posts.append(post)
+    return posts
